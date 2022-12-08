9f04f84705af7c32a457f87a4fbf5504c0d0e274
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2022-12-09 00:14:31 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,82 @@
+public class Main {
+    public static void main (String[] args) {
+        int numeroIf= 1;
+
+        if ( numeroIf > 0) {
+            System.out.println (numeroIf + "Es positivo");
+        } 
+        
+        else if (numeroIf < 0) {
+        System.out.println (numeroIf + "Es negativo");
+     while (numeroIf> 0);
+        System.out.println (numeroIf +"Es positivo");
+    
+    }
+} 
+
+
+
+    int numeroWhile = -10; {
+    while (numeroWhile <3) {
+        System.out.println (numeroWhile +1);
+
+    }
+
+}
+
+{ 
+    int numeroDoWhile =2;
+    do { 
+
+        System.out.println(numeroDoWhile);
+        numeroDoWhile=numeroDoWhile+1;
+    } while (numeroDoWhile < 3);
+}
+
+{ for (int numeroFor=0; numeroFor <=3; numeroFor= numeroFor +1) {
+    System.out.println (numeroFor);
+}
+
+} 
+
+{ var Estacion="INVIERNO";
+switch (Estacion)
+
+{ 
+    case "PRIMAVERA":
+         System.out.println ("Es PRIMAVERA");
+    break;
+    case "VERANO":
+         System.out.println ("ES VERANO");
+    break;
+    case "OTONO":
+         System.out.println ("ES OTONO");
+    break;
+    case "INVIERNO":
+         System.out.println ("ES INVIERNO");
+    break;
+    default:
+         System.out.println ("No es una Estacion");
+    break; 
+
+}
+
+{
+    System.out.println ("Fin del Switch Case");
+
+    }
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+    
+
