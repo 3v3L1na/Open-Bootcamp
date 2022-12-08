c276b3b2550c22fc289f7f60a338727254e20c81
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2022-12-09 00:13:02 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main  {
+  
+     public static void main(String[] args) {
+        int resultado;
+        resultado= suma (10, 30, 14 );
+        System.out.println(resultado);
+    }
+
+public static int suma (int a, int b, int d) {
+    return a + b + d;
+}
+
+}
